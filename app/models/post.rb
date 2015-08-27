389c9edac7cdf6c519d9de2ca8de4674b15236e4
wengkhing <kheryee@live.com>
2015-08-27 16:49:15 +0800

    Implement all funtions

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ActiveRecord::Base
+  # Remember to create a migration!
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+
+  validates :content, :presence => true
+  validates :title, :presence => true
+  validates :author, :presence => true
+end
